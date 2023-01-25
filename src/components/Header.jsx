a5f965ace338802df92436bff3b60af69b98731d
David Weinberg <>
2023-01-25 23:27:32 +0200

    first commit: init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css"
+
+function Header(){
+    return(
+        <>
+        <header className="app-header">
+            <h1>POKE BATTLE</h1>
+            <h3>Challange your self!</h3>
+            <h3>Test your knowlage</h3>
+        </header>
+        </>
+    )
+}
+
+export default Header
